67596e19895bcf7ad0c8f2203df41307319df4f1
PKill27 <Porter.killian@gmail.com>
2017-07-17 16:09:06 -0700

    Merge branch 'master' of https://github.com/rtolsma/Ziggurat-Internship

---
+++ frontmain.js
@@ -0,0 +1,4 @@
+//frontmain.js
+
+
+init();
--- webfunctions.js
+++ webfunctions.js
@@ -5,44 +5,25 @@ easily separated to clean up Porter's shit messy code.
 */
 
 function calculateMovingAverage(dt,row) {
-     // calculate average of closing value for last x days,
-                // if we are x or more days into the data set
-                if (row >= days - 1) {
-                    var total = 0;
-                    for (var i = 0; i < days; i++) {
-                        total += dt.getValue(row - i, 3);
-                    }
-                    var avg = total / days;
-                    return {
-                        v: avg
-                        , f: avg.toFixed(2)
-                    };
-                }
-                else {
-                    // return null for < x days
-                    return null;
-                }
-            }
-
-function httpGetAsync(theUrl, callback){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+    // calculate average of closing value for last x days,
+    // if we are x or more days into the data set
+    if (row >= days - 1) {
+        var total = 0;
+        for (var i = 0; i < days; i++) {
+            total += dt.getValue(row - i, 3);
         }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
-function setData(HttpRequest){
-    stockData=JSON.parse(HttpRequest);
-    displayedData.length = 0;
-    for(var i=stockData.length-100;i<stockData.length;i++){
-        displayedData.push(stockData[i]);
-
+        var avg = total / days;
+        return {
+            v: avg
+            , f: avg.toFixed(2)
+        };
     }
-    console.log(displayedData, "object");
-
+    else {
+        // return null for < x days
+        return null;
+    }
 }
+
 
 
 function objectToArray(object){
@@ -61,7 +42,27 @@ function changeTime(minutes){
     }
 }
 
+function combine(candles){
+    var time=candles[0].time;
+    var close=0;
+    var open=0;
+    var high=0;
+    var low=Infinity;
+    var volume=0;
+    if(candles.length>0) {
+        open=candles[0].open;
+        close=candles[candles.length-1].close;
+        for(var i=0; i<candles.length; i++){
+            if(candles[i].high>high){ high=candles[i].high;}
+            if(candles[i].low<low){ low=candles[i].low;}
+        }
+    }
+    var date=new Date(time);
+    return {"time":date, "low":low, "open":open, "close":close,"high": high};
+}
 
 function init() {
+    google.charts.load('current', {'packages': ['corechart']});//loads all of the functions required by the charts
+    google.charts.setOnLoadCallback(drawChart);
 
 }
