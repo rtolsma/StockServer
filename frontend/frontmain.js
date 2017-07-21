9091215bb3897d9573da41f81a6f01f302bb99bf
rshortsma <1tolsmar@gmail.com>
2017-07-21 10:55:59 -0700

    changed div

--- frontend/frontmain.js
+++ frontend/frontmain.js
@@ -1,7 +1,7 @@
 //frontmain.js
 
 
-function init() {
+/*function init() {
     google.charts.load('current', {'packages': ['corechart']});//loads all of the functions required by the charts
     google.load("visualization", "1", {packages:["corechart"]});
 
